a0547be8746c88da79e8bb22d4da07077b455793
Elizabeth F. Ortiz <ortiz.beth58@gmail.com>
2017-11-15 13:34:54 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1029 @@
+$(document).ready(function(){
+
+
+	
+	function frame(f){
+		switch(f){
+			case 1:
+			return	$("#frame1").animate({
+					bottom: "+=-350px"
+				}, 6000).promise();
+				break;
+			case 2:
+			return	$("#frame2").animate({
+					bottom: "+=-350px"
+				}, 9000).promise();
+				break;
+			case 3:
+			return	$("#frame3").animate({
+					bottom: "+=-350px"
+				}, 12000).promise();
+				break;
+			case 4:
+			return	$("#frame4").animate({
+					bottom: "+=-350px"
+				}, 15000).promise();
+				break;
+			case 5:
+			return	$("#frame5").animate({
+					bottom: "+=-350px"
+				}, 6000).promise();
+				break;
+			case 6:
+			return	$("#frame6").animate({
+					bottom: "+=-350px"
+				}, 6000).promise();
+				break;
+			case 7:
+			return	$("#frame7").animate({
+					bottom: "+=-350px"
+				}, 6000).promise();
+				break;
+			case 8:
+			return	$("#frame8").animate({
+					bottom: "+=-350px"
+				}, 6000).promise();
+				break;
+			case 9:
+			return	$("#frame9").animate({
+					bottom: "+=-350px"
+				}, 6000).promise();
+				break;
+			case 10:
+			return	$("#frame10").animate({
+					bottom: "+=-350px"
+				}, 6000).promise();
+				break;
+			case 11:
+			return	$("#frame11").animate({
+					bottom: "+=-350px"
+				}, 6000).promise();
+				break;
+			case 12:
+			return	$("#frame12").animate({
+					bottom: "+=-350px"
+				}, 6000).promise();
+				break;
+			default:
+		}
+	}
+
+
+	function fr(f){
+		switch(f){
+			case 1:
+				$("#fr1").css("opacity","1");
+			return	$("#fr1").animate({
+					bottom: "+=-350px"
+				}, 1000).promise();
+				break;
+			case 2:
+				$("#fr2").css("opacity","1");
+			return	$("#fr2").animate({
+					bottom: "+=-350px"
+				}, 1000).promise();
+				break;
+			case 3:
+				$("#fr3").css("opacity","1");
+			return	$("#fr3").animate({
+					bottom: "+=-350px"
+				}, 1000).promise();
+				break;
+			case 4:
+				$("#fr4").css("opacity","1");
+			return	$("#fr4").animate({
+					bottom: "+=-350px"
+				}, 1000).promise();
+				break;
+			case 5:
+				$("#fr5").css("opacity","1");
+			return	$("#fr5").animate({
+					bottom: "+=-350px"
+				}, 1000).promise();
+				break;
+			case 6:
+				$("#fr6").css("opacity","1");
+			return	$("#fr6").animate({
+					bottom: "+=-350px"
+				}, 1000).promise();
+				break;
+			case 7:
+				$("#fr7").css("opacity","1");
+			return	$("#fr7").animate({
+					bottom: "+=-350px"
+				}, 1000).promise();
+				break;
+			case 8:
+				$("#fr8").css("opacity","1");
+			return	$("#fr8").animate({
+					bottom: "+=-350px"
+				}, 1000).promise();
+				break;
+			case 9:
+				$("#fr9").css("opacity","1");
+			return	$("#fr9").animate({
+					bottom: "+=-350px"
+				}, 1000).promise();
+				break;
+			case 10:
+				$("#fr10").css("opacity","1");
+			return	$("#fr10").animate({
+					bottom: "+=-350px"
+				}, 1000).promise();
+				break;
+			case 11:
+				$("#fr11").css("opacity","1");
+			return	$("#fr11").animate({
+					bottom: "+=-350px"
+				}, 1000).promise();
+				break;
+			case 12:
+				$("#fr12").css("opacity","1");
+			return	$("#fr12").animate({
+					bottom: "+=-350px"
+				}, 1000).promise();
+				break;
+			default:
+		}
+	}
+
+
+	function rrr(r){
+		switch(r){
+			case 1:
+			return	$("#rrr1").animate({
+					bottom: "+=350px"
+					}, 1000).promise();
+				break;
+			case 2:
+			return	$("#rrr2").animate({
+					bottom: "+=350px"
+				}, 1000).promise();
+				break;
+			case 3:
+			return	$("#rrr3").animate({
+					bottom: "+=350px"
+				}, 1000).promise();
+				break;
+			case 4:
+			return	$("#rrr4").animate({
+					bottom: "+=350px"
+				}, 1000).promise();
+				break;
+			case 5:
+			return	$("#rrr5").animate({
+					bottom: "+=350px"
+				}, 3000).promise();
+				break;
+			case 6:
+			return	$("#rrr6").animate({
+					bottom: "+=350px"
+				}, 3000).promise();
+				break;
+			case 7:
+			return	$("#rrr7").animate({
+					bottom: "+=350px"
+				}, 3000).promise();
+				break;
+			case 8:
+			return	$("#rrr8").animate({
+					bottom: "+=350px"
+				}, 3000).promise();
+				break;
+			case 9:
+			return	$("#rrr9").animate({
+					bottom: "+=350px"
+				}, 3000).promise();
+				break;
+			case 10:
+			return	$("#rrr10").animate({
+					bottom: "+=350px"
+				}, 3000).promise();
+				break;
+			case 11:
+			return	$("#rrr11").animate({
+					bottom: "+=350px"
+				}, 3000).promise();
+				break;
+			case 12:
+			return	$("#rrr12").animate({
+					bottom: "+=350px"
+				}, 3000).promise();
+				break;
+			default:
+		}
+	}
+
+
+	function rr(r){
+		switch(r){
+			case 1:
+			return	$("#rr1").animate({
+					bottom: "+=350px"
+					}, 6000).promise();
+				break;
+			case 2:
+			return	$("#rr2").animate({
+					bottom: "+=350px"
+				}, 6000).promise();
+				break;
+			case 3:
+			return	$("#rr3").animate({
+					bottom: "+=350px"
+				}, 6000).promise();
+				break;
+			case 4:
+			return	$("#rr4").animate({
+					bottom: "+=350px"
+				}, 6000).promise();
+				break;
+			case 5:
+			return	$("#rr5").animate({
+					bottom: "+=350px"
+				}, 6000).promise();
+				break;
+			case 6:
+			return	$("#rr6").animate({
+					bottom: "+=350px"
+				}, 6000).promise();
+				break;
+			case 7:
+			return	$("#rr7").animate({
+					bottom: "+=350px"
+				}, 6000).promise();
+				break;
+			case 8:
+			return	$("#rr8").animate({
+					bottom: "+=350px"
+				}, 6000).promise();
+				break;
+			case 9:
+			return	$("#rr9").animate({
+					bottom: "+=350px"
+				}, 6000).promise();
+				break;
+			case 10:
+			return	$("#rr10").animate({
+					bottom: "+=350px"
+				}, 6000).promise();
+				break;
+			case 11:
+			return	$("#rr11").animate({
+					bottom: "+=350px"
+				}, 6000).promise();
+				break;
+			case 12:
+			return	$("#rr12").animate({
+					bottom: "+=350px"
+				}, 6000).promise();
+				break;
+			default:
+		}
+	}
+
+
+	function rej(r){
+		switch(r){
+			case 1:
+			return	$("#rej1").animate({
+					bottom: "+=350px"
+				}, 500).promise();
+				break;
+			case 2:
+			return	$("#rej2").animate({
+					bottom: "+=350px"
+				}, 500).promise();
+				break;
+			case 3:
+			return	$("#rej3").animate({
+					bottom: "+=350px"
+				}, 500).promise();
+				break;
+			case 4:
+			return	$("#rej4").animate({
+					bottom: "+=350px"
+				}, 500).promise();
+				break;
+			case 5:
+			return	$("#rej5").animate({
+					bottom: "+=350px"
+				}, 500).promise();
+				break;
+			case 6:
+			return	$("#rej6").animate({
+					bottom: "+=350px"
+				}, 500).promise();
+				break;
+			case 7:
+			return	$("#rej7").animate({
+					bottom: "+=350px"
+				}, 500).promise();
+				break;
+			case 8:
+			return	$("#rej8").animate({
+					bottom: "+=350px"
+				}, 500).promise();
+				break;
+			case 9:
+			return	$("#rej9").animate({
+					bottom: "+=350px"
+				}, 500).promise();
+				break;
+			case 10:
+			return	$("#rej10").animate({
+					bottom: "+=350px"
+				}, 500).promise();
+				break;
+			case 11:
+			return	$("#rej11").animate({
+					bottom: "+=350px"
+				}, 500).promise();
+				break;
+			case 12:
+			return	$("#rej12").animate({
+					bottom: "+=350px"
+				}, 500).promise();
+				break;
+			default:
+		}
+	}
+
+
+	function re(r){
+		switch(r){
+			case 1:
+				$("#re1").css("opacity","1");
+				break;
+			case 2:
+				$("#re2").css("opacity","1");
+				break;
+			case 3:
+				$("#re3").css("opacity","1");
+				break;
+			case 4:
+				$("#re4").css("opacity","1");
+				break;
+			case 5:
+				$("#re5").css("opacity","1");
+				break;
+			case 6:
+				$("#re6").css("opacity","1");
+				break;
+			case 7:
+				$("#re7").css("opacity","1");
+				break;
+			case 8:
+				$("#re8").css("opacity","1");
+				break;
+			case 9:
+				$("#re9").css("opacity","1");
+				break;
+			case 10:
+				$("#re10").css("opacity","1");
+				break;
+			case 11:
+				$("#re11").css("opacity","1");
+				break;
+			case 12:
+				$("#re12").css("opacity","1");
+				break;
+			default:
+		}
+	}
+
+
+	function ref(r){
+		switch(r){
+			case 1:
+				$("#ref1").css("opacity","0");
+				break;
+			case 2:
+				$("#ref2").css("opacity","0");
+				break;
+			case 3:
+				$("#ref3").css("opacity","0");
+				break;
+			case 4:
+				$("#ref4").css("opacity","0");
+				break;
+			case 5:
+				$("#ref5").css("opacity","0");
+				break;
+			case 6:
+				$("#ref6").css("opacity","0");
+				break;
+			case 7:
+				$("#ref7").css("opacity","0");
+				break;
+			case 8:
+				$("#ref8").css("opacity","0");
+				break;
+			case 9:
+				$("#ref9").css("opacity","0");
+				break;
+			case 10:
+				$("#ref10").css("opacity","0");
+				break;
+			case 11:
+				$("#ref11").css("opacity","0");
+				break;
+			case 12:
+				$("#ref12").css("opacity","0");
+				break;
+			default:
+		}
+	}
+
+
+	function window(){
+		return $("#window").animate({
+				left: "+=30px"
+			}, 200).promise();
+	}
+
+						
+	var flag = new Array(12);
+	for(var x=0;x<12;x++){
+		flag[x] = false;
+	}
+
+	var rrflag = new Array(12);
+	for(var x=0;x<12;x++){
+		rrflag[x] = false;
+	}
+
+	$("#frame1").click(function(){
+		$("#frame1").css("opacity","0");
+		flag[0] = true;
+	});
+	$("#frame2").click(function(){
+		$("#frame2").css("opacity","0");
+		flag[1] = true;
+	});
+	$("#frame3").click(function(){
+		$("#frame3").css("opacity","0");
+		flag[2] = true;
+	});
+	$("#frame4").click(function(){
+		$("#frame4").css("opacity","0");
+		flag[3] = true;
+	});
+	$("#frame5").click(function(){
+		$("#frame5").css("opacity","0");
+		flag[4] = true;
+	});
+	$("#frame6").click(function(){
+		$("#frame6").css("opacity","0");
+		flag[5] = true;
+	});
+	$("#frame7").click(function(){
+		$("#frame7").css("opacity","0");
+		flag[6] = true;
+	});
+	$("#frame8").click(function(){
+		$("#frame8").css("opacity","0");
+		flag[7] = true;
+	});
+	$("#frame9").click(function(){
+		$("#frame9").css("opacity","0");
+		flag[8] = true;
+	});
+	$("#frame10").click(function(){
+		$("#frame10").css("opacity","0");
+		flag[9] = true;
+	});
+	$("#frame11").click(function(){
+		$("#frame11").css("opacity","0");
+		flag[10] = true;
+	});
+	$("#frame12").click(function(){
+		$("#frame12").css("opacity","0");
+		flag[11] = true;
+	});
+
+
+
+	$("#rr1").click(function(){
+		$("#rr1").css("opacity","0");
+		rrflag[0] = true;
+	});
+	$("#rr2").click(function(){
+		$("#rr2").css("opacity","0");
+		rrflag[1] = true;
+	});
+	$("#rr3").click(function(){
+		$("#rr3").css("opacity","0");
+		rrflag[2] = true;
+	});
+	$("#rr4").click(function(){
+		$("#rr4").css("opacity","0");
+		rrflag[3] = true;
+	});
+	$("#rr5").click(function(){
+		$("#rr5").css("opacity","0");
+		rrflag[4] = true;
+	});
+	$("#rr6").click(function(){
+		$("#rr6").css("opacity","0");
+		rrflag[5] = true;
+	});
+	$("#rr7").click(function(){
+		$("#rr7").css("opacity","0");
+		rrflag[6] = true;
+	});
+	$("#rr8").click(function(){
+		$("#rr8").css("opacity","0");
+		rrflag[7] = true;
+	});
+	$("#rr9").click(function(){
+		$("#rr9").css("opacity","0");
+		rrflag[8] = true;
+	});
+	$("#rr10").click(function(){
+		$("#rr10").css("opacity","0");
+		rrflag[9] = true;
+	});
+	$("#rr11").click(function(){
+		$("#rr11").css("opacity","0");
+		rrflag[10] = true;
+	});
+	$("#rr12").click(function(){
+		$("#rr12").css("opacity","0");
+		rrflag[11] = true;
+	});
+
+
+	function rrfix(r){
+		switch(r){
+			case 1:
+				fr(1).done(function(){
+					rrr(1).done(function(){
+						return;
+					});
+				});
+				break;
+			case 2:
+				fr(2).done(function(){
+					rrr(2).done(function(){
+						return;
+					});
+				});
+				break;
+			case 3:
+				fr(3).done(function(){
+					rrr(3).done(function(){
+						return;
+					});
+				});
+				break;
+			case 4:
+				fr(4).done(function(){
+					rrr(4).done(function(){
+						return;
+					});
+				});
+				break;
+			case 5:
+				fr(5).done(function(){
+					rrr(5).done(function(){
+						return;
+					});
+				});
+				break;
+			case 6:
+				fr(6).done(function(){
+					rrr(6).done(function(){
+						return;
+					});
+				});
+				break;
+			case 7:
+				fr(7).done(function(){
+					rrr(7).done(function(){
+						return;
+					});
+				});
+				break;
+			case 8:
+				fr(8).done(function(){
+					rrr(8).done(function(){
+						return;
+					});
+				});
+				break;
+			case 9:
+				fr(9).done(function(){
+					rrr(9).done(function(){
+						return;
+					});
+				});
+				break;
+			case 10:
+				fr(10).done(function(){
+					rrr(10).done(function(){
+						return;
+					});
+				});
+				break;
+			case 11:
+				fr(11).done(function(){
+					rrr(11).done(function(){
+						return;
+					});
+				});
+				break;
+			case 12:
+				fr(12).done(function(){
+					rrr(12).done(function(){
+						return;
+					});
+				});
+				break;
+
+			default:
+		}
+	}
+
+
+
+	function fix(r){
+		switch(r){
+			case 1:
+				rej(1).done(function(){
+					fr(1).done(function(){
+						return;
+					});
+				});
+				break;
+			case 2:
+				rej(2).done(function(){
+					fr(2).done(function(){
+						return;
+					});
+				});
+				break;
+			case 3:
+				rej(3).done(function(){
+					fr(3).done(function(){
+						return;
+					});
+				});
+				break;
+			case 4:
+				rej(4).done(function(){
+					fr(4).done(function(){
+						return;
+					});
+				});
+				break;
+			case 5:
+				rej(5).done(function(){
+					fr(5).done(function(){
+						return;
+					});
+				});
+				break;
+			case 6:
+				rej(6).done(function(){
+					fr(6).done(function(){
+						return;
+					});
+				});
+				break;
+			case 7:
+				rej(7).done(function(){
+					fr(7).done(function(){
+						return;
+					});
+				});
+				break;
+			case 8:
+				rej(8).done(function(){
+					fr(8).done(function(){
+						return;
+					});
+				});
+				break;
+			case 9:
+				rej(9).done(function(){
+					fr(9).done(function(){
+						return;
+					});
+				});
+				break;
+			case 10:
+				rej(10).done(function(){
+					fr(10).done(function(){
+						return;
+					});
+				});
+				break;
+			case 11:
+				rej(11).done(function(){
+					fr(11).done(function(){
+						return;
+					});
+				});
+				break;
+			case 12:
+				rej(12).done(function(){
+					fr(12).done(function(){
+						return;
+					});
+				});
+				break;
+			default:
+		}
+	}
+
+	function notfix(r){
+		switch(r){
+			case 1:
+				re(1);
+				ref(1);
+				break;
+			case 2:
+				re(2);
+				ref(2);
+				break;
+			case 3:
+				re(3);
+				ref(3);
+				break;
+			case 4:
+				re(4);
+				ref(4);
+				break;
+			case 5:
+				re(5);
+				ref(5);
+				break;
+			case 6:
+				re(6);
+				ref(6);
+				break;
+			case 7:
+				re(7);
+				ref(7);
+				break;
+			case 8:
+				re(8);
+				ref(8);
+				break;
+			case 9:
+				re(9);
+				ref(9);
+				break;
+			case 10:
+				re(10);
+				ref(10);
+				break;
+			case 11:
+				re(11);
+				ref(11);
+				break;
+			case 12:
+				re(12);
+				ref(12);
+				break;
+			default:
+		}
+	}
+////////////////////////////////////////////////////////////////////////////
+	frame(1).done(function(){
+		if(flag[0]){
+			fix(1);
+		}
+
+		notfix(1);
+
+		rr(1).done(function(){
+
+			if(rrflag[0]){
+				rrfix(1);
+			}
+
+			window().done(function(){
+
+				frame(5).done(function(){
+					if(flag[4]){
+						fix(5);
+					}
+
+					notfix(5);
+
+					rr(5).done(function(){
+						if(rrflag[4]){
+							rrfix(5)
+						}
+
+						window().done(function(){
+
+							frame(9).done(function(){
+								if(flag[8]){
+									fix(9);
+								}
+
+								notfix(9);
+
+								rr(9).done(function(){
+									if(rrflag[8]){
+										rrfix(9);
+									}
+								});
+							});
+						});
+					});
+				});
+			});
+		});
+	});
+
+
+
+	frame(2).done(function(){
+		if(flag[1]){
+			fix(2);
+		}
+
+		notfix(2);
+
+		rr(2).done(function(){
+			if(rrflag[1]){
+				rrfix(2);
+			}
+
+			window().done(function(){
+
+				frame(6).done(function(){
+					if(flag[5]){
+						fix(6);
+					}
+
+					notfix(6);
+
+					rr(6).done(function(){
+
+						if(rrflag[5]){
+							rrfix(6);
+						}
+
+						window().done(function(){
+
+							frame(10).done(function(){
+								if(flag[9]){
+									fix(10);
+								}
+
+								notfix(10);
+
+								rr(10).done(function(){
+									if(rrflag[9]){
+										rrfix(10);
+									}
+								});
+							});
+						});
+					});
+				});
+			});
+		});
+	});
+
+
+
+
+
+	frame(3).done(function(){
+		if(flag[2]){
+			fix(3);
+		}
+
+		notfix(3)
+
+		rr(3).done(function(){
+
+			if(rrflag[2]){
+				rrfix(3);
+			}
+
+			window().done(function(){
+
+				frame(7).done(function(){
+
+					if(flag[6]){
+						fix(7);
+					}
+
+					notfix(7);
+
+					rr(7).done(function(){
+
+						if(rrflag[6]){
+							rrfix(7);
+						}
+
+						window().done(function(){
+
+							frame(11).done(function(){
+								if(flag[10]){
+									fix(11);
+								}
+
+								notfix(11);
+
+								rr(11).done(function(){
+									if(rrflag[10]){
+										rrfix(11);
+									}
+								});
+							});
+						});
+					});
+				});
+			});
+		});
+	});
+
+
+
+
+
+
+
+
+
+	frame(4).done(function(){
+		if(flag[3]){
+			fix(4);
+		}
+
+		notfix(4);
+
+		rr(4).done(function(){
+
+			if(rrflag[3]){
+				rrfix(4);
+			}
+
+			window().done(function(){
+
+				frame(8).done(function(){
+					if(flag[7]){
+						fix(8);
+					}
+
+					notfix(8);
+
+					rr(8).done(function(){
+
+						if(rrflag[7]){
+							rrfix(8);
+						}
+
+						window().done(function(){
+
+							frame(12).done(function(){
+								if(flag[11]){
+									fix(12);
+								}
+
+								notfix(12);
+
+								rr(12).done(function(){
+									if(rrflag[11]){
+										rrfix(12);
+									}
+								});
+							});
+						});
+					});
+				});
+			});
+		});
+	});
+
+
+
+	
+
+
+	/*window.setTimeout(function(){ // timer 
+		$("html").html("<p>wtf</p>")
+	},10000);
+
+
+
+	if( $("#frame1").css("bottom") < "200px"){
+		$("#rr1").animate({
+			bottom: "+=350px"
+		}, 4000);
+	}
+	
+	if( $("#rr1").css("bottom") > "300px" ){
+		$("#window").animate({
+			left: "+=30px"
+		}, 200);
+	}*/
+
+});
